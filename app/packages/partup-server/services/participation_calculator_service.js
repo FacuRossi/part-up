78a2e6ca363e19f098c9b76f961ec6affbcbde85
Noel Heesen <noelheesen@gmail.com>
2018-06-12 16:56:00 +0200

    fix(scores): Adjust calculation of user participation score

--- app/packages/partup-server/services/participation_calculator_service.js
+++ app/packages/partup-server/services/participation_calculator_service.js
@@ -1,3 +1,6 @@
+import moment, { isMoment } from 'moment';
+import _ from 'lodash';
+
 var d = Debug('services:participation_calculator');
 
 /**
@@ -21,43 +24,68 @@ Partup.server.services.participation_calculator = {
         d('Supports recent partups score for user [' + upperId + '] is ' + score2 + '/100 and counts for 25% → ' + (score2 * score2weight) + '/100');
         score += score2 * score2weight;
 
-        var score3 = this._calculateAverageContributionRatingScore(upper);
-        var score3weight = 0.25;
-        d('Average contribution rating score for user [' + upperId + '] is ' + score3 + '/100 and counts for 25% → ' + (score3 * score3weight) + '/100');
+        var score3 = this._calculateAmountOfUpdatesCreated(upper);
+        var score3weight = 0.17;
+        d(`Amount of updates created in the last 30 days score for user ${upper._id} is ${score3}/100 and counts for 17% ${score3*score3weight}/100`);
         score += score3 * score3weight;
 
-        var score4 = this._calculateActiveContributionsScore(upper);
-        var score4weight = 0.25;
-        d('Active contributions score for user [' + upperId + '] is ' + score4 + '/100 and counts for 25% → ' + (score4 * score4weight) + '/100');
+        var score4 = this._calculateAmountOfComments(upper);
+        var score4weight = 0.08;
+        d(`Amount of comments created by the current user in the past 30 days for user ${upper._id} is ${score4}/100 and counts for 8% ${score4*score4weight}/100`);
         score += score4 * score4weight;
+
+        var score5 = this._calculateAmountOfContributions(upper);
+        var score5weight = 0.25;
+        d(`Amount of verified contributions within the last 30 days, ${upper._id} is ${score5}/100 and counts for 25% ${score5*score5weight}/100`);
+        score += score5 * score5weight;
 
         d('Total participation score for user [' + upperId + '] is ' + score + '/100');
 
         return Math.min(100, score);
     },
 
-    _calculateActiveContributionsScore: function(upper) {
-        var activeContributionsScore = 0;
+    _calculateAmountOfComments({ _id }) {
+      const scoreDelta = 4;
+      let commentsScore = 0;
 
-        // By using a score delta of 4, an upper can receive the full
-        // 100 score of this part by having 25 active contributions
-        var scoreDelta = 4;
-
-        var contributions = Contributions.find({upper_id: upper._id, verified:true});
-
-        contributions.forEach(function(contribution) {
-            var partup = Partups.findOne(contribution.partup_id);
-
-            if (partup && !partup.hasEnded()) {
-                activeContributionsScore += scoreDelta;
+      const minimumDate = moment().subtract(30, 'days').toISOString();
+      Updates
+        .find({ comments_count: { $gte: 0 }, comments: { $elemMatch: { 'creator._id': _id, 'created_at': { $gte: minimumDate } } } })
+        .forEach((update) => {
+          _.get(update, 'comments', []).forEach(({ creator_id, created_at }) => {
+            if (creator_id === _id && created_at >= minimumDate) {
+              commentsScore += scoreDelta;
             }
+          });
         });
 
-        return Math.min(100, activeContributionsScore);
+      return Math.min(100, commentsScore);
     },
 
-    _calculateAverageContributionRatingScore: function(upper) {
-        return Math.min(100, upper.average_rating || 0);
+    _calculateAmountofContributions({ _id }) {
+      const scoreDelta = 4;
+      let contributionsScore = 0;
+
+      const minimumDate = moment().subtract(30, 'days').toISOString();
+
+      Contributions
+        .find({ upper_id: _id, verified: true, created_at: { $gte: minimumDate } })
+        .forEach((contribution) => { contributionsScore += scoreDelta; });
+
+      return Math.min(100, contributionsScore);
+    },
+
+    _calculateAmountOfUpdatesCreated({ _id }) {
+      let updatesScore = 0;
+      const scoreDelta = 4;
+
+      const minimumDate = moment().subtract(30, 'days').toISOString();
+
+      Updates
+        .find({ upper_id: _id, created_at: { $gte: minimumDate } })
+        .forEach(() => { updatesScore += scoreDelta });
+
+      return Math.min(100, updatesScore);
     },
 
     _calculateSupportsRecentPartupsScore: function(upper) {
