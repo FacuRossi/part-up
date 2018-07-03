eb7f4584a52ddace13a69fbda0bc0decca3be37e
Noel Heesen <noelheesen@gmail.com>
2018-07-03 14:26:16 +0200

    fix(notifcations): no longer group notifications for mobile

--- app/packages/partup-lib/collections/notifications.js
+++ app/packages/partup-lib/collections/notifications.js
@@ -34,20 +34,33 @@ Notifications.findForUser = function(user, selector, options) {
     return Notifications.find(selector, options);
 };
 
+Notifications.findForUserForMobile = function(user, selector, options) {
+  if (!user) return;
+
+  var selector = selector || {};
+  var options = options || {};
+
+  selector.for_upper_id = user._id;
+  options.limit = Math.min(options.limit, 100);
+  options.sort = {created_at: -1};
+
+  return Notifications.find(selector, options);
+};
+
 Notifications.deleteForCreator = function(user) {
     if (!user) return;
 
     // Set the name to deleted user to anonimize notifications
-    Notifications.update( 
+    Notifications.update(
         {'type_data.creator._id': user._id},
-        {"$set": {"type_data.creator.name": "Deleted User"}}, 
+        {"$set": {"type_data.creator.name": "Deleted User"}},
         {"multi": true}
     )
 
     // Set the name to deleted user to anonimize notifications
-    Notifications.update( 
+    Notifications.update(
         {'type_data.upper._id': user._id},
-        {"$set": {"type_data.upper.name": "Deleted User"}}, 
+        {"$set": {"type_data.upper.name": "Deleted User"}},
         {"multi": true}
     )
 }
--- app/packages/partup-server/publications/notifications.js
+++ app/packages/partup-server/publications/notifications.js
@@ -1,13 +1,20 @@
-Meteor.publishComposite('notifications.for_upper', function(limit) {
+Meteor.publishComposite('notifications.for_upper', function(limit, mobile = false) {
     check(limit, Number);
     this.unblock();
 
     var user = Meteor.users.findOne(this.userId);
     if (!user) return;
 
+    let notificationCursor;
+    if (mobile) {
+      notificationCursor = Notifications.findForUserForMobile(user, {}, { limit });
+    } else {
+      notificationCursor = Notifications.findForUser(user, {}, { limit });
+    }
+
     return {
         find: function() {
-            return Notifications.findForUser(user, {}, {limit: limit});
+            return notificationCursor;
         },
         children: [
             {find: Images.findForNotification}
